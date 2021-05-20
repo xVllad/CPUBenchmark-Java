de3d4451e82a08e4492f8d05c3f605d755d17212
deyoooh <andrei.hojda00@gmail.com>
2021-05-20 17:10:22 +0300

    Added UI

---
+++ src/Controller/UI_Controller.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import javafx.scene.control.ChoiceBox;
+
+public class UI_Controller {
+
+    public ChoiceBox<String> choicebox1;
+
+    public void initialize()
+    {
+        choicebox1.getItems().addAll("2000", "4000","6000","8000","10000","15000","20000");
+    }
+
+
+}
--- src/testbench/TestCPUDigitsOfPi.java
+++ src/testbench/TestCPUDigitsOfPi.java
@@ -1,13 +1,28 @@
 package testbench;
 import bench.IBenchmark;
 import bench.cpu.CPUDigitsOfPi;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import logging.ConsoleLogger;
 import logging.ILog;
 import logging.TimeUnit;
 import timing.ITimer;
 import timing.Timer;
 
-public class TestCPUDigitsOfPi {
+import java.util.Objects;
+
+public class TestCPUDigitsOfPi extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("resources/Bench_page.fxml"));
+        primaryStage.setTitle("CPU Benchmark");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
         ITimer timer = new Timer();
         ILog log = new ConsoleLogger();
@@ -27,5 +42,6 @@ public class TestCPUDigitsOfPi {
         log.close();
         System.out.println("Available processors (cores): " +
                 Runtime.getRuntime().availableProcessors());
+        launch(args);
     }
 }
